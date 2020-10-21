6c818f7eff3be3cb5bd947d43aedd8933df8866f
Jeevan Reddy Ragula <jeevanreddy_ragula@mcafee.com>
2020-10-21 19:31:19 +0530

    Initial commit

---
+++ src/main/java/com/test/sendgrid/SendGridExample.java
@@ -0,0 +1,36 @@
+package com.test.sendgrid;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import java.io.IOException;
+
+public class SendGridExample {
+
+    public static void main(String[] args) throws IOException {
+        Email from = new Email("jeevanragula@gmail.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("jeevanragula@gmail.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+}
